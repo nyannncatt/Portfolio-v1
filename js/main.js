20ed9b0f8de0e22e36245b71a0823fe200e7d0b7
nyannncatt <kamirelax187@gmail.com>
2025-11-16 18:22:17 +0800

    h

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Smooth scrolling for navigation links
+// Smooth scrolling for navigation linksz
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
